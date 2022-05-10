a577017c60f39a587b29e7679e3ab8f1f07bd99d
SShipon <nxshipon@gmail.com>
2022-05-10 20:01:39 +0600

    updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000;
 
-// middleware
+// middleware add 
 
 app.use(cors())
 app.use(express.json());
@@ -67,7 +67,7 @@ async function run() {
         
         });
 
-     //delete
+     //delete api 
         app.delete('/user/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
